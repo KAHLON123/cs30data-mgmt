6a6111388b561ef83243b305e2fd663141717389
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-04-20 11:23:21 -0600

    add

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Data Management</title>
+</head>
+<body>
+<h1>da  mall</h1>
+<h2>les gooo</h2>
+<h1>Choose your items: </h1>
+<img src="img/walmart-logo.jpeg"><br />
+<img src="img/daCar.png" width="500"><br />
+<img src="img/vin-diesel.jpg"> <br />
+<img src="img/coffee.jpg" width="500"> <br />
+<img src="img/food-img.jpg" width="500">
+<?php
+
+?>
+
+</body>
+</html>
