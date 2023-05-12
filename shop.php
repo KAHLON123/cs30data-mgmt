5083b963197f0d88d966ddcf2ced9c7addc8e3f0
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-05-12 11:24:07 -0600

    Update shop.php

--- shop.php
+++ shop.php
@@ -24,29 +24,30 @@ if (isset($_GET['logout'])) {
 <h1>da  mall</h1>
 <h2>les gooo</h2>
 <img src="img/walmart-logo.jpeg"><br />
-<h1>Toggle items into/out of cart: </h1>
+<h1>OPTIONS: </h1>
 <section>
     <form action="shop.php" method="POST">
-    <select name="s">
-        <option value="1">Add to favourites</option>
-        <option value="2">Remove from favourites</option>
-        <option value="3">Filter by </option>
-        <option value="4">Alphabetize</option>
-        <option value="5">Only display favourites </option>
-    </select>
+        <label>Car</label>
+        <input type="checkbox" value="car" name="option[]">
+        <label>Diesel</label>
+        <input type="checkbox" value="diesel" name="option[]">
+        <label>Coffee</label>
+        <input type="checkbox" value="coffee" name="option[]">
+        <label>Food</label>
+        <input type="checkbox" value="food" name="option[]">
+        <label>Hat 1</label>
+        <input type="checkbox" value="hat1" name="option[]"><br />
+        <select name="s">
+            <option value="1">Add to favourites</option>
+            <option value="2">Remove from favourites</option>
+            <option value="3">Filter by </option>
+            <option value="4">Alphabetize</option>
+            <option value="5">Only display favourites </option>
+        </select><br />
         <input type="submit" value="submit" name="submit"><br />
     </form>
 </section>
-<label>Car</label>
-<input type="checkbox" value="car" name="option[]">
-<label>Diesel</label>
-<input type="checkbox" value="diesel" name="option[]">
-<label>Coffee</label>
-<input type="checkbox" value="coffee" name="option[]">
-<label>Food</label>
-<input type="checkbox" value="food" name="option[]">
-<label>Hat 1</label>
-<input type="checkbox" value="hat1" name="option[]">
+
 <?php
 // display all items
  $disAllSql = "SELECT img FROM items WHERE 1";
@@ -58,10 +59,12 @@ if (isset($_GET['logout'])) {
 if (isset($_POST['submit'])){
     switch ($_POST['s']){
         case "1":
-            add($conn);
+            $checkboxArr = $_POST['option'];
+            add($conn, $checkboxArr);
             break;
         case "2":
-            remove($conn);
+            $checkboxArr = $_POST['option'];
+            remove($conn, $checkboxArr);
             break;
         case "3":
             filterItems();
@@ -80,30 +83,32 @@ if (isset($_POST['logout'])) {
     header("location: register.php");
 }
 
-function add($conn){
+function add($conn, $favToAdd){
     // parse array of favourite item IDs 
     $curr_user = $_SESSION["loggedas"];
     $getFavSql = "SELECT favourites FROM users WHERE username = '$curr_user'";
     $favQuery = mysqli_query($conn, $getFavSql);
-    $favArr = mysqli_fetch_all($favQuery);
-    var_dump($favArr);
-// TURN FAVARR INTO STRING USING FOREACH PUSH INTO ARRAY
 
-    //$oldFavArr = json_decode(mysqli_fetch_all($favQuery));
-    // get values to add
+    if ($favQuery){ // if !empty
+        $favArr = fetchIntoArr($favQuery);
+    } else {
+        $favArr = [];
+    }
 
-    // $favArr = $_POST['option[]'];
-    // foreach ($favArr as $item){
-    //     array_push($oldFavArr, $item);
-    // }
+    // put new values into array from sql
+    foreach ($favToAdd as $item){
+        array_push($favArr, $item);
+    }
 
     //delete duplicate values and insert into user favourite list
-    $finalFavArr = json_encode(array_unique($oldFavArr));
+
+    $favStr = json_encode(array_unique($favArr));
+    echo $favStr;
     $sql = "UPDATE users SET favourites = '$favStr' WHERE username = '$curr_user'";
     $query = mysqli_query($conn, $getFavSql);
 }
 function remove($conn){
-
+    //for each element in array if its eqal to value to delete, remove from arrray
     $sql = "UPDATE users SET favourites = '$'";
 }
 function filterItems(){
@@ -120,16 +125,25 @@ function alphabetItems($itemsArr){
      }
 }
 function displayFav($conn){
-$curr_user = $_SESSION["loggedas"];
-$getFavSql = "SELECT favourites FROM users WHERE username = '$curr_user'";
-$favQuery = mysqli_query($conn, $getFavSql);
-$favItemsArr = mysqli_fetch_all($favQuery);
- foreach($favItemsArr as $itemArr) {
-    echo "<img src='img/" . $itemArr[0] . "' width='300'><br />";
- }
+    $curr_user = $_SESSION["loggedas"];
+    $getFavSql = "SELECT favourites FROM users WHERE username = '$curr_user'";
+    $favQuery = mysqli_query($conn, $getFavSql);
+    $favItemsArr = mysqli_fetch_all($favQuery);
+    foreach($favItemsArr as $itemArr) {
+        echo "<img src='img/" . $itemArr[0] . "' width='300'><br />";
+    }
 }
 
 // HELPERS?
+function fetchIntoArr($query){
+    $temp = mysqli_fetch_all($query);
+    $arr = [];
+    foreach ($temp as $value){
+        array_push($arr, $value[0]);
+    }
+    return $arr; 
+}
+
 function bubblesort($arr){
     for ($i = 1; $i < count($arr);$i++ ) {
         for ($n = 0; $n < count($arr) - 1; $n++) {
